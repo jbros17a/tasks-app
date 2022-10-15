434776e382d2e4ee02820d1298621e3fc2dea154
Jordi Bros <jordibros@MacBook-Pro-de-Jordi.local>
2022-10-15 17:25:41 +0900

    accounts admin

--- task_app/accounts/admin.py
+++ task_app/accounts/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from accounts.models import UserAccount
+from accounts.forms import UserCreationForm, UserChangeForm
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+    list_display = ('email', 'username', 'first_name', 'last_name', 'date_joined', 'last_login', 'is_staff')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('username', 'first_name', 'last_name')}),
+        ('Activity', {'fields': ('date_joined', 'last_login',)}),
+        ('Permissions', {'fields': ('is_staff',)}),
+    )
+    add_fieldsets = (
+        (None, {'fields': ('email', 'password1', 'password2')}),
+        ('Personal info', {'fields': ('username', 'first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_staff',)}),
+    )
+    search_fields = ('email', 'username')
+    readonly_fields = ('id', 'date_joined', 'last_login')
+
+admin.site.register(UserAccount, UserAdmin)
