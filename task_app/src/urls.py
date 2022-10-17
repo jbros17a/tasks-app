4b16f364dba873a5683b796a7fe51863adb6023e
Jordi Bros <jordibros@MacBook-Pro-de-Jordi.local>
2022-10-17 17:33:18 +0900

    crud functionality

--- task_app/src/urls.py
+++ task_app/src/urls.py
@@ -4,5 +4,9 @@ from src.views import *
 app_name = 'tasks'
 
 urlpatterns = [
-    path('', TaskList.as_view(), name='main')
+    path('', TaskList.as_view(), name='main'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('create-task/', CreateTask.as_view(), name='create'),
+    path('task-edit/<int:pk>/', UpdateTask.as_view(), name='update'),
+    path('task-delete/<int:pk>/', DeleteTask.as_view(), name='delete'),
 ]
--- task_app/src/views.py
+++ task_app/src/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 from .models import Task
 
@@ -8,3 +9,30 @@ class TaskList(ListView):
     model = Task
     template_name = 'task_list.html'
     context_object_name = 'tasks'
+
+
+class TaskDetail(DetailView):
+    model = Task
+    template_name = 'task_detail.html'
+    context_object_name = 'task'
+
+
+class CreateTask(CreateView):
+    model = Task
+    template_name = 'task_form.html'
+    fields = '__all__'
+    success_url = reverse_lazy('tasks:main')
+
+
+class UpdateTask(UpdateView):
+    model = Task
+    template_name = 'task_form.html'
+    fields = '__all__'
+    success_url = reverse_lazy('tasks:main')
+
+
+class DeleteTask(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks:main')
+    template_name = 'task_delete_confirm.html'
